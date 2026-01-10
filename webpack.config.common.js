23bcb32b4d3d2bb4b51f49df2d79346736af3605
Tatiana <kartasheva.tatiana@gmail.com>
2026-01-10 19:10:16 +0300

    build: add CopyWebpackPlugin

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
 const path = require('path');
 
 module.exports = {
@@ -12,6 +13,11 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, 'public/index.html')
-        })
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "public/assets", to: "dist" }
+            ],
+        }),
     ]
 }
