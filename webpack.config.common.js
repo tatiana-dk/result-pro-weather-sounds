e22bb51e8c30d18a98e07ac03f26e371147aa3bc
Tatiana <kartasheva.tatiana@gmail.com>
2026-01-10 19:01:10 +0300

    build: add entry/output

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -1,1 +1,11 @@
-module.exports = {}
+const path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    }
+}
